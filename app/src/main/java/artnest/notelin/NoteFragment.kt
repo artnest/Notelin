db8f5b31f1fa292f454dc40c2941dc7276a812b7
artnest <artem291997@gmail.com>
2017-06-04 23:37:15 +0300

    Moved views to AnkoComponent Added listener to title text field

--- app/src/main/java/artnest/notelin/NoteFragment.kt
+++ app/src/main/java/artnest/notelin/NoteFragment.kt
@@ -3,18 +3,31 @@ package artnest.notelin
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import org.jetbrains.anko.AnkoContext
+import org.jetbrains.anko.sdk25.coroutines.textChangedListener
 import org.jetbrains.anko.support.v4.ctx
 
 class NoteFragment : Fragment() {
+
+    lateinit var noteFragmentUI : NoteFragmentUI
 
     var mNote = Note()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        noteFragmentUI = NoteFragmentUI()
     }
 
-    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?) =
-            NoteFragmentUI().createView(AnkoContext.create(ctx, this))
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?) : View {
+        val v = noteFragmentUI.createView(AnkoContext.create(ctx, this))
+        noteFragmentUI.title.textChangedListener {
+            onTextChanged {
+                text, start, before, count -> mNote.mTitle = text.toString()
+            }
+        }
+        return v
+    }
 }
--- app/src/main/java/artnest/notelin/NoteFragmentUI.kt
+++ app/src/main/java/artnest/notelin/NoteFragmentUI.kt
@@ -1,13 +1,15 @@
 package artnest.notelin
 
+import android.widget.EditText
 import org.jetbrains.anko.*
 
 class NoteFragmentUI : AnkoComponent<NoteFragment> {
 
+    lateinit var title : EditText
+
     override fun createView(ui: AnkoContext<NoteFragment>) = with(ui) {
         verticalLayout {
-            var title = editText {
-                id = R.id.note_title
+            title = editText {
                 hintResource = R.string.note_title_hint
             }
         }
