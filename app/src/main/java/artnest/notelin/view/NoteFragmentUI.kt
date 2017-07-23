2ebc4e57605c25abac676a9177303b33ec0c33b2
artnest <artem291997@gmail.com>
2017-07-23 13:05:06 +0300

    Changed main screen layout Added Anko Upgraded libraries versions

--- app/src/main/java/artnest/notelin/view/NoteFragmentUI.kt
+++ app/src/main/java/artnest/notelin/view/NoteFragmentUI.kt
@@ -5,18 +5,27 @@ import android.widget.EditText
 import artnest.notelin.R
 import artnest.notelin.controller.NoteFragment
 import org.jetbrains.anko.*
+import org.jetbrains.anko.design.floatingActionButton
 
 class NoteFragmentUI : AnkoComponent<NoteFragment> {
 
     lateinit var title : EditText
 
     override fun createView(ui: AnkoContext<NoteFragment>) = with(ui) {
-        verticalLayout {
+        relativeLayout {
             padding = dip(30)
             title = editText {
                 hintResource = R.string.note_title_hint
                 inputType = TYPE_CLASS_TEXT
                 maxLines = 1
+            }
+
+            floatingActionButton {
+                imageResource = android.R.drawable.ic_menu_add
+            }.lparams {
+                margin = dip(16)
+                alignParentBottom()
+                alignParentRight()
             }
         }
     }
